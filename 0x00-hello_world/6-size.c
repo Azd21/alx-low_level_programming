5f0fa8ec2acf4e7dd32243d98b6a7be841860d6c
Alazar <alazarzdemissie21@gmail.com>
2022-04-07 23:20:32 +0300

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * 
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+pirntf("Size of a char: %d byte(s)\n", sizeof(char));
+pirntf("Size of an int: %d byte(s)\n", sizeof(int));
+pirntf("Size of a long int: %d byte(s)\n", sizeof(long int));
+pirntf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+pirntf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
