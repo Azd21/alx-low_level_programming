d0b8e3b445c11c12896113446536021aac7e0e81
Alazar <alazarzdemissie21@gmail.com>
2022-04-07 23:11:27 +0300

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Hello, printf task
  * Return: return 1 after the program execute
  */
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
