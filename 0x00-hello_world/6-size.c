6374035024221f41f8d4d50b49b1174d87c02bfb
Alazar <alazarzdemissie21@gmail.com>
2022-04-07 23:35:31 +0300

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point
+ *
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-pirntf("Size of a char: %d byte(s)\n", sizeof(char));
-pirntf("Size of an int: %d byte(s)\n", sizeof(int));
-pirntf("Size of a long int: %d byte(s)\n", sizeof(long int));
-pirntf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-pirntf("Size of a float: %d byte(s)\n", sizeof(float));
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+pirntf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+pirntf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+pirntf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+pirntf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long (c));
+pirntf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
