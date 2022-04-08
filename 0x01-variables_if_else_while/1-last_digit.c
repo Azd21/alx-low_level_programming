81d17318364c1160a489446d0e0a134001c4b1b1
Alazar <alazarzdemissie21@gmail.com>
2022-04-08 20:04:24 +0300

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -23,7 +22,7 @@ printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 
 else if (n % 10 == 0)
 {
-printf ("Last digit of %i is %i and is 0\n", n, n % 10);
+printf("Last digit of %i is %i and is 0\n", n, n % 10);
 }
 
 else
